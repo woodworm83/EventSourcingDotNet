3a682bfdff381db117fc7425f951ffde190e7e26
Reto Kühni <reto.kuehni@osterwalder.com>
2022-11-08 06:16:02 +0100

    Add dependency injection support

---
+++ src/Streamy.InMemory/RegistrationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Streamy.InMemory;
+
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddInMemoryEventStore<TAggregateId>(
+        this IServiceCollection services)
+        where TAggregateId : IAggregateId
+        => services
+            .AddSingleton<InMemoryEventStore<TAggregateId>>()
+            .AddSingleton<IEventStore<TAggregateId>>(
+                serviceProvider => serviceProvider.GetRequiredService<InMemoryEventStore<TAggregateId>>())
+            .AddSingleton<IEventPublisher<TAggregateId>>(
+                serviceProvider => serviceProvider.GetRequiredService<InMemoryEventStore<TAggregateId>>());
+}
---
+++ src/Streamy/RegistrationExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Streamy;
+
+public static class RegistrationExtensions
+{
+    public static IServiceCollection AddEventSourcing(this IServiceCollection services)
+        => services
+            .AddTransient(typeof(IAggregateRepository<,>), typeof(AggregateRepository<,>));
+}
