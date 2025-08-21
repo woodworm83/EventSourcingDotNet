5e232b4826bfb2bd54dd02aa09fe31d19ae2e7c5
Reto Kühni <reto.kuehni@georgfischer.com>
2025-08-21 08:57:52 +0200

    Add Update method to AggregateRepository which takes an aggregate instead of a aggregate id

--- src/EventSourcingDotNet/IAggregateRepository.cs
+++ src/EventSourcingDotNet/IAggregateRepository.cs
@@ -8,7 +8,7 @@ namespace EventSourcingDotNet;
 /// <typeparam name="TAggregateId">The aggregate identifier</typeparam>
 /// <typeparam name="TState">The aggregate state</typeparam>
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
-public interface IAggregateRepository<TAggregateId, TState> 
+public interface IAggregateRepository<TAggregateId, TState>
     where TAggregateId : IAggregateId
     where TState : IAggregateState<TState, TAggregateId>, new()
 {
@@ -38,6 +38,30 @@ public interface IAggregateRepository<TAggregateId, TState>
     /// <summary>
     /// In-place update of an aggregate.
     /// </summary>
+    /// <param name="aggregate">The aggregate to be updated</param>
+    /// <param name="correlationId">The correlation id of the process</param>
+    /// <param name="causationId">The causation id which initiated the events</param>
+    /// <param name="events">The events to be appended to the event store</param>
+    /// <returns>The aggregate with updated version and uncommitted events cleared to allow additional changes without reloading.</returns>
+    /// <exception cref="OptimisticConcurrencyException">
+    /// Thrown when expected version of the aggregate does not match actual version of the event store.
+    /// This indicates that new events were added to the stream since the aggregate was replayed.
+    /// </exception>
+    sealed async Task<Aggregate<TAggregateId, TState>> UpdateAsync(
+        Aggregate<TAggregateId, TState> aggregate,
+        CorrelationId? correlationId,
+        CausationId? causationId,
+        params IEnumerable<IDomainEvent> events)
+        => await SaveAsync(
+            events.Aggregate(
+                aggregate,
+                (a, @event) => a.AddEvent(@event)),
+            correlationId,
+            causationId).ConfigureAwait(false);
+
+    /// <summary>
+    /// In-place update of an aggregate.
+    /// </summary>
     /// <param name="id">The id of the aggregate</param>
     /// <param name="correlationId">The correlation id of the process</param>
     /// <param name="causationId">The causation id which initiated the events</param>
@@ -48,16 +72,16 @@ public interface IAggregateRepository<TAggregateId, TState>
     /// This indicates that new events were added to the stream since the aggregate was replayed.
     /// </exception>
     sealed async Task<Aggregate<TAggregateId, TState>> UpdateAsync(
-        TAggregateId id, 
-        CorrelationId? correlationId, 
-        CausationId? causationId, 
+        TAggregateId id,
+        CorrelationId? correlationId,
+        CausationId? causationId,
         params IEnumerable<IDomainEvent> events)
-        => await SaveAsync(
-            events.Aggregate(
-                await GetByIdAsync(id).ConfigureAwait(false), 
-                (aggregate, @event) => aggregate.AddEvent(@event)),
-            correlationId,
-            causationId).ConfigureAwait(false);
+        => await UpdateAsync(
+                await GetByIdAsync(id).ConfigureAwait(false),
+                correlationId,
+                causationId,
+                events)
+            .ConfigureAwait(false);
 
     /// <summary>
     /// In-place update of an aggregate.
