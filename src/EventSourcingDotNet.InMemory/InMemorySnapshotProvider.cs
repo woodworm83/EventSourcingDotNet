113d42a3def0c1e62c0adea2c6a634760f6e036c
Reto Kühni <reto.kuehni@georgfischer.com>
2025-08-25 15:03:21 +0200

    Fix in memory snapshot provider

--- src/EventSourcingDotNet.InMemory/InMemorySnapshotProvider.cs
+++ src/EventSourcingDotNet.InMemory/InMemorySnapshotProvider.cs
@@ -6,16 +6,7 @@ namespace EventSourcingDotNet.InMemory;
 public sealed class InMemorySnapshotProvider : ISnapshotProvider
 {
     public void RegisterServices(IServiceCollection services, Type aggregateIdType, Type stateType)
-    {
-        var snapshotType = typeof(InMemorySnapshotStore<,>).MakeGenericType(aggregateIdType, stateType);
-        var serviceType = typeof(ISnapshotStore<,>).MakeGenericType(aggregateIdType, stateType);
-
-        services
-            .AddSingleton(snapshotType)
-            .AddSingleton(typeof(IHostedService), ImplementationFactory)
-            .AddSingleton(serviceType, ImplementationFactory);
-        
-        object ImplementationFactory(IServiceProvider serviceProvider) =>
-            serviceProvider.GetRequiredService(snapshotType);
-    }
+        => services.AddSingleton(
+            typeof(ISnapshotStore<,>).MakeGenericType(aggregateIdType, stateType),
+            typeof(InMemorySnapshotStore<,>).MakeGenericType(aggregateIdType, stateType));
 }
